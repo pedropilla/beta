853ac2a15eb46d0f83b565de622aa1a9bbd5f9bb
jasperdg <jasperdegooijer@gmail.com>
2021-01-26 13:55:22 +0100

    adjuseted distribution hints calculation

--- src/utils/calcDistributionHint.ts
+++ src/utils/calcDistributionHint.ts
@@ -1,17 +1,10 @@
 import Big from "big.js";
 
-/**
- * Computes the distribution hint that should be used for setting the initial odds to `initialOdds`
- * @param initialOdds - an array of numbers proportional to the initial estimate of the probability of each outcome
- */
+// Using Omen's utility functions adjusted where necessary 
+
 export const calcDistributionHint = (initialOdds: number[]): Big[] => {
     if (initialOdds.some(x => x === 0)) {
         throw new Error("Invalid probability - can't assign a probability of zero to an outcome");
-    }
-
-    const allEqual = initialOdds.every(x => x === initialOdds[0]);
-    if (allEqual) {
-        return [];
     }
 
     const initialOddsBig = initialOdds.map(x => new Big(x));
